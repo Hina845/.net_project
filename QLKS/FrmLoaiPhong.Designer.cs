b7abdcdd10a8766abdedf63055437a30e7ee9ba8
Laptop TTC <linhpham270401@gmail.com>
2021-10-20 17:23:00 +0700

    Thu

---
+++ QLKS/FrmLoaiPhong.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace QLKS
+{
+    partial class FrmLoaiPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.AutoSize = true;
+            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitle.Location = new System.Drawing.Point(365, 21);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(196, 33);
+            this.lbltitle.TabIndex = 0;
+            this.lbltitle.Text = "LOẠI PHÒNG";
+            // 
+            // FrmLoaiPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 536);
+            this.Controls.Add(this.lbltitle);
+            this.Name = "FrmLoaiPhong";
+            this.Text = "FrmLoaiPhong";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitle;
+    }
+}
