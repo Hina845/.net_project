ed792b439ddfac3d01c4c7bccde258bac60b70d2
DELL <iamgiang>
2021-10-24 10:25:23 +0700

    đặt phòng

--- QLKS/Frm_DATPHONG.Designer.cs
+++ QLKS/Frm_DATPHONG.Designer.cs
@@ -30,25 +30,221 @@ namespace QLKS
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_khachang = new System.Windows.Forms.NumericUpDown();
+            this.txt_datphong = new System.Windows.Forms.NumericUpDown();
+            this.txt_songuoi = new System.Windows.Forms.NumericUpDown();
+            this.txt_sophong = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btn_chitietdatphong = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_khachang)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_datphong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_songuoi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_sophong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(536, 41);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(190, 32);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "ĐẶT PHÒNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID Đặt Phòng:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID Khách Hàng:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ID NV Thực Hiện:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Số Phòng:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ngày Đặt:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(350, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ngày Đến:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(350, 170);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Ngày đi:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(50, 170);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Số Người:";
+            // 
+            // txt_khachang
+            // 
+            this.txt_khachang.Location = new System.Drawing.Point(178, 97);
+            this.txt_khachang.Name = "txt_khachang";
+            this.txt_khachang.Size = new System.Drawing.Size(120, 26);
+            this.txt_khachang.TabIndex = 9;
+            // 
+            // txt_datphong
+            // 
+            this.txt_datphong.Location = new System.Drawing.Point(178, 129);
+            this.txt_datphong.Name = "txt_datphong";
+            this.txt_datphong.Size = new System.Drawing.Size(120, 26);
+            this.txt_datphong.TabIndex = 10;
+            // 
+            // txt_songuoi
+            // 
+            this.txt_songuoi.Location = new System.Drawing.Point(178, 164);
+            this.txt_songuoi.Name = "txt_songuoi";
+            this.txt_songuoi.Size = new System.Drawing.Size(120, 26);
+            this.txt_songuoi.TabIndex = 11;
+            // 
+            // txt_sophong
+            // 
+            this.txt_sophong.Location = new System.Drawing.Point(178, 197);
+            this.txt_sophong.Name = "txt_sophong";
+            this.txt_sophong.Size = new System.Drawing.Size(120, 26);
+            this.txt_sophong.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(439, 99);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(113, 26);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(439, 129);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(113, 26);
+            this.dateTimePicker2.TabIndex = 14;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker3.Location = new System.Drawing.Point(439, 161);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(113, 26);
+            this.dateTimePicker3.TabIndex = 15;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(488, 197);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(64, 26);
+            this.numericUpDown1.TabIndex = 16;
+            // 
+            // btn_chitietdatphong
+            // 
+            this.btn_chitietdatphong.Location = new System.Drawing.Point(129, 267);
+            this.btn_chitietdatphong.Name = "btn_chitietdatphong";
+            this.btn_chitietdatphong.Size = new System.Drawing.Size(152, 40);
+            this.btn_chitietdatphong.TabIndex = 17;
+            this.btn_chitietdatphong.Text = "Chi tiết đặt phòng";
+            this.btn_chitietdatphong.UseVisualStyleBackColor = true;
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Location = new System.Drawing.Point(305, 267);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(94, 40);
+            this.btn_thoat.TabIndex = 18;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
             // 
             // Frm_DATPHONG
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 676);
+            this.ClientSize = new System.Drawing.Size(602, 330);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_chitietdatphong);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txt_sophong);
+            this.Controls.Add(this.txt_songuoi);
+            this.Controls.Add(this.txt_datphong);
+            this.Controls.Add(this.txt_khachang);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Frm_DATPHONG";
             this.Text = "Frm_DATPHONG";
+            ((System.ComponentModel.ISupportInitialize)(this.txt_khachang)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_datphong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_songuoi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_sophong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +253,23 @@ namespace QLKS
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown txt_khachang;
+        private System.Windows.Forms.NumericUpDown txt_datphong;
+        private System.Windows.Forms.NumericUpDown txt_songuoi;
+        private System.Windows.Forms.NumericUpDown txt_sophong;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btn_chitietdatphong;
+        private System.Windows.Forms.Button btn_thoat;
     }
 }
