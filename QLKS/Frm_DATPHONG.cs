d500fc8f26abf5a757478e9df41801781fc02420
DELL <iamgiang>
2021-10-29 11:33:38 +0700

    đã thêm phần Combobox + tự động nhảy ID đặt phòng

--- QLKS/Frm_DATPHONG.cs
+++ QLKS/Frm_DATPHONG.cs
@@ -30,6 +30,16 @@ namespace QLKS
         {
             txt_khachang.DataBindings.Clear();
             txt_khachang.Value = ID;
+            //chọn ra ID lớn nhất ở bảng DAT_PHONG
+            DataTable dtaID = kn.Lay_DulieuBang("select MAX(ID) AS ID from DAT_PHONG ");
+            //tự động nhảy ID đặt phòng
+            txt_datphong.DataBindings.Add("Value", dtaID, "ID");
+            txt_datphong.Value = txt_datphong.Value + 1;
+
+            //combobox Nhanvienthuchien
+            txt_nhanvienthuchien.DataSource = kn.Lay_DulieuBang("select ID from NHAN_VIEN where CHUC_VU='Tiep tan';");
+            txt_nhanvienthuchien.DisplayMember = "ID";
+            txt_nhanvienthuchien.ValueMember = "ID";
         }
 
         private void btn_chitietdatphong_Click(object sender, EventArgs e)
@@ -38,6 +48,10 @@ namespace QLKS
             decimal c = txt_sophong.Value;
             int b = decimal.ToInt32(a);
             int d = decimal.ToInt32(c);
+
+            //them dl vao bang DAT_PHONG
+            String sql = "Insert into DAT_PHONG VALUES("+txt_datphong.Value+","+txt_khachang.Value+","+txt_nhanvienthuchien.Text+","+txt_songuoi.Value+","+txt_sophong.Value+",'"+txt_ngaydat.Value+"','"+txt_ngayden.Value+"','"+txt_ngaydi+"')";
+            kn.ThucThi(sql);
                 
             Frm_DANGKIPHONG frm = new Frm_DANGKIPHONG(b, d);
             frm.Show();
