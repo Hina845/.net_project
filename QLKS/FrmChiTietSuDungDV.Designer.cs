14a795a8266eca7bb8eaa04c10ecf547849f7955
Tran Vinh Hoang <hoang.tran@amela.vn>
2021-11-07 09:54:21 +0700

    Fix

--- QLKS/FrmChiTietSuDungDV.Designer.cs
+++ QLKS/FrmChiTietSuDungDV.Designer.cs
@@ -200,7 +200,7 @@ namespace QLKS
             // 
             this.dateNgayDat.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 13F);
             this.dateNgayDat.Checked = false;
-            this.dateNgayDat.CustomFormat = "dd-MM-yyy";
+            this.dateNgayDat.CustomFormat = "yyy-MM-dd";
             this.dateNgayDat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateNgayDat.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateNgayDat.Location = new System.Drawing.Point(439, 158);
